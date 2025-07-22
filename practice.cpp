f46afbc0c9f86df08513e90f1227cdb619d65d6c
rakeshgon <leomat366@gmail.com>
2025-07-23 04:47:47 +0530

    Add files via upload

---
+++ practice.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+ int main(){
+    int n ;
+    cout<<"enter the number"<<endl;
+    cin>>n;
+     
+    int  i = 1;
+    while(i <= n){
+        int j  = 1;
+        while(j <= i){
+            char ch = 'A' + i - 1;
+            cout<<char( ch + j - 1 );
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+ }
